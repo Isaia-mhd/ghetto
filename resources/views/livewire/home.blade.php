3675c111d8dab5fab6e0e636c1ebfba4908f5595
Isaia-mhd <mohamedesaie21@gmail.com>
2025-10-07 10:06:14 +0300

    feat: create best_listing section for home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,4 +2,5 @@
     @include("components.home.hero")
     @include("components.home.last_listing")
     @include("components.home.nearby_listing")
+    @include("components.home.best_listing")
 </div>
