0096bf79d0d460cf9302c3f5db60e4e58f86229f
Isaia-mhd <mohamedesaie21@gmail.com>
2025-10-09 11:29:21 +0300

    fix: fix the issue of color on header

--- resources/views/components/mobile_menu.blade.php
+++ resources/views/components/mobile_menu.blade.php
@@ -1,16 +1,16 @@
 <div id="mobile-menu"
-    class="fixed top-0 right-0 h-full w-64 bg-primary text-accent font-semibold p-6 transform translate-x-full transition-transform duration-300 lg:hidden z-100">
+    class="fixed top-0 right-0 h-full w-64 bg-primary text-myblack font-semibold p-6 transform translate-x-full transition-transform duration-300 lg:hidden z-100">
     <button id="close-btn" class="text-red-300 mb-6 cursor-pointer">
         ✖
     </button>
-    <ul class="flex flex-col space-y-4 text-xs">
+    <ul class="flex flex-col space-y-4 text-xs text-mywhite">
         <a href="{{ route('home') }}" wire:navigate>Accueil</a>
         <a href="{{ route("property") }}" wire:navigate>Maisons</a>
         <a href="/contact" wire:navigate>Telecharger l'App Mobile</a>
         <a href="/" wire:navigate>FAQs</a>
         <a href="/contact" wire:navigate>Contact</a>
         <a href="{{ route("contact") }}" wire:navigate>A propos</a>
-        <a href="{{ route("login") }}" wire:navigate class="bg-secondary font-semibold text-sm py-2 px-3 rounded-full">Connexion</a>
+        <a href="{{ route("login") }}" wire:navigate class="bg-secondary font-semibold text-xs text-mywhite py-2 px-3 rounded-full">Connexion</a>
         @auth
             <a href="{{ route("profile") }}" wire:navigate
                 class="w-20 h-10 hidden border-2 py-2 border-primary hover:border-secondary duration-700 lg:flex justify-center items-center gap-2 rounded-full">
