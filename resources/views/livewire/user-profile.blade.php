67e5ec7846f8d5a3c4a99018f597517d8b10e082
Isaia-mhd <mohamedesaie21@gmail.com>
2025-10-09 12:10:32 +0300

    fix(modal): fix the issue of modal and it's style

--- resources/views/livewire/user-profile.blade.php
+++ resources/views/livewire/user-profile.blade.php
@@ -44,28 +44,7 @@
 
             {{-- Deconnexion Modal --}}
             @if ($showModal)
-                <div class="fixed inset-0 flex items-center justify-center z-50">
-                    <!-- Fond semi-transparent -->
-                    <div class="absolute inset-0 bg-black bg-opacity-50" wire:click="closeModal"></div>
-
-                    <!-- Contenu de la modal -->
-                    <div class="bg-white rounded-xl shadow-lg p-6 w-96 z-10">
-                        <h2 class="text-lg font-semibold text-gray-800 mb-4">Confirmation</h2>
-                        <p class="text-gray-600 mb-6">Êtes-vous sûr de vouloir vous déconnecter ?</p>
-
-                        <div class="flex justify-end gap-4">
-                            <button wire:click="closeModal"
-                                class="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 transition">
-                                Annuler
-                            </button>
-
-                            <button wire:click="logout"
-                                class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition">
-                                Se déconnecter
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                @include("components.auth.modal_logout")
             @endif
             {{-- User profil --}}
             @if ($isProfile)
